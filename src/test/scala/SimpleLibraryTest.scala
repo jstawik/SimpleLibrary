a7820972ddbed231ca4259e889a86588fc51b049
jstawik <jakub.stawik@gmail.com>
2020-02-02 21:38:36 +0100

    Refactor tests, add more tests

--- src/test/scala/SimpleLibraryTest.scala
+++ src/test/scala/SimpleLibraryTest.scala
@@ -1,37 +1,27 @@
 import org.scalatest.funsuite.AnyFunSuite
 
-object SimpleLibraryTest extends AnyFunSuite {
+class SimpleLibraryTest extends AnyFunSuite {
  test("Adding and removing and searching books") {
-  val lib = new SimpleLibrary
-  lib.addBook(BookData("Art of War", -450, "Sun Tzu"))
-  lib.addBook(BookData("Maelstrom", 2001, "Peter Watts"))
-  lib.addBook(BookData("Najwyższa jakość", 2010, "Cacao DecoMorreno"))
-  lib.addBook(BookData("Najwyższa jakość", 2010, "Cacao DecoMorreno"))
-  lib.addBook(BookData("Najwyższa jakość", 2010, "Cacao DecoMorreno"))
-  lib.rmBook(2)
-  lib.rmBook(3)
-  assert(lib.searchBook().size == 3)
+   val lib = new SimpleLibrary
+   lib.addBook(BookData("Art of War", -450, "Sun Tzu"))
+   lib.addBook(BookData("Maelstrom", 2001, "Peter Watts"))
+   lib.addBook(BookData("Najwyższa jakość", 2010, "Cacao DecoMorreno"))
+   lib.addBook(BookData("Najwyższa jakość", 2010, "Cacao DecoMorreno"))
+   lib.addBook(BookData("Najwyższa jakość", 2010, "Cacao DecoMorreno"))
+   lib.rmBook(2)
+   lib.rmBook(3)
+   assert(lib.searchBook().size == 3)
+   assert(lib.searchBook(titlePat = "Maelstrom") == Nil)
+   assert(lib.searchBook(authorPat = "Cacao DecoMorreno").size == 2)
  }
- test("Check if book is removed correctly"){
-  val lib = new SimpleLibrary
-  lib.addBook(BookData("Art of War", -450, "Sun Tzu"))
-  lib.addBook(BookData("Maelstrom", 2001, "Peter Watts"))
-  lib.addBook(BookData("Najwyższa jakość", 2010, "Cacao DecoMorreno"))
-  lib.addBook(BookData("Najwyższa jakość", 2010, "Cacao DecoMorreno"))
-  lib.addBook(BookData("Najwyższa jakość", 2010, "Cacao DecoMorreno"))
-  lib.rmBook(2)
-  lib.rmBook(3)
-  assert(lib.searchBook(titlePat = "Maelstrom") == Nil)
- }
- test("Check if book other copies of the book are preserved"){
-  val lib = new SimpleLibrary
-  lib.addBook(BookData("Art of War", -450, "Sun Tzu"))
-  lib.addBook(BookData("Maelstrom", 2001, "Peter Watts"))
-  lib.addBook(BookData("Najwyższa jakość", 2010, "Cacao DecoMorreno"))
-  lib.addBook(BookData("Najwyższa jakość", 2010, "Cacao DecoMorreno"))
-  lib.addBook(BookData("Najwyższa jakość", 2010, "Cacao DecoMorreno"))
-  lib.rmBook(2)
-  lib.rmBook(3)
-  assert(lib.searchBook(authorPat="Cacao DecoMorreno").size == 2)
- }
+  test("Book lending"){
+    val lib = new SimpleLibrary
+    lib.addBook(BookData("Art of War", -450, "Sun Tzu"))
+    lib.addBook(BookData("Maelstrom", 2001, "Peter Watts"))
+    lib.addBook(BookData("Najwyższa jakość", 2010, "Cacao DecoMorreno"))
+    lib.addBook(BookData("Najwyższa jakość", 2010, "Cacao DecoMorreno"))
+    lib.addBook(BookData("Najwyższa jakość", 2010, "Cacao DecoMorreno"))
+    lib.lendBook(IdGenerator.getTopId, "Jakub")
+    assert(lib.bookDetails(IdGenerator.getTopId).get.toOption.get.name == "Jakub")
+  }
 }
